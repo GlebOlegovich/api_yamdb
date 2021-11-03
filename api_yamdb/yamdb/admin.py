d035495e51002e6cc59b7d9079c3d9bab106ed3e
GlebOlegovich <godleib@yandex.ru>
2021-11-03 15:38:52 +0300

    Модели готовы

--- api_yamdb/yamdb/admin.py
+++ api_yamdb/yamdb/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Genre, Titles, Genre_title
+from .models import User, Genre, Titles, Genre_title, Comment, Review
 
 admin.site.register(User)
 admin.site.register(Genre_title)
 admin.site.register(Titles)
 admin.site.register(Genre)
+admin.site.register(Comment)
+admin.site.register(Review)
--- api_yamdb/yamdb/models.py
+++ api_yamdb/yamdb/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from api_yamdb.settings import ROLE
 
 
@@ -60,9 +61,46 @@ class Genre_title(models.Model):
         return f'"{self.title}" относится к жанру : {self.genre}'
 
     class Meta:
+        
         constraints = [
             models.UniqueConstraint(
                 fields=['title_id', 'genre_id'],
                 name='genre_titles'
             )
         ]
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Titles, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        default_related_name = 'reviews'
+        verbose_name_plural = 'Отзывы'
+        verbose_name = 'Отзыв'
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments')
+
+    class Meta:
+        default_related_name = 'comments'
+        verbose_name_plural = 'Коментарии к отзывам'
+        verbose_name = 'Коментарий к отзыву'
