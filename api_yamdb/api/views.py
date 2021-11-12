55c8f0995fe55974aafc1f193d181e51af3684a7
SubbotinaOlga <osubbotina146@gmail.com>
2021-11-12 23:59:26 +0300

    undestand documentations

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, status, viewsets
+from rest_framework import filters, status, viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
@@ -67,8 +67,11 @@ class CategoryViewSet(viewsets.ModelViewSet):
     lookup_url_kwarg = 'slug'
 
 
-@action(detail=True, methods=['GET', 'POST', 'DEL', 'PATCH'])
-class GenreViewSet(viewsets.ModelViewSet):
+@action(detail=True, methods=['LIST', 'POST', 'DEL'])
+class GenreViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = [IsAdminOrReadOnly]
@@ -77,27 +80,6 @@ class GenreViewSet(viewsets.ModelViewSet):
     search_fields = ('name',)
     lookup_field = 'slug'
     lookup_url_kwarg = 'slug'
-
-    def retrieve(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-        except Exception:
-            return Response(None,
-                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        serializer = GenreSerializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, *args, **kwargs):
-        try:
-            instance = self.get_object()
-        except Exception:
-            return Response(None,
-                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        serializer = GenreSerializer(instance, data=request.data,
-                                     partial=True)
-        serializer.is_valid(raise_exception=False)
-        self.perform_update(serializer)
-        return Response(serializer.data)
 
 
 class TitlesViewSet(viewsets.ModelViewSet):
